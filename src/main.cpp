6fcbb59a1294c9c33ec1477971da4d013564507b
Cilan <abraham.hamidi01@gmail.com>
2017-02-18 19:00:29 -0600

    fixed servicelocator

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,21 @@
 #include "LocalResources.h"
 
 int main(int argc, char** argv) {
+    const float_t scale = .6;
+    const uint16_t windowDim = (uint16_t) (std::min(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height) * scale);
+
     Locator::provideArgs(argv[0]);
 
-    sf::Window window(sf::VideoMode(1280, 800), "I Am Synthetic", sf::Style::Titlebar + sf::Style::Close);
     Locator::provide(std::make_unique<LocalResources>());
-    Locator::getResource() -> loadTexture("test");
+    sf::Texture splash = Locator::getResource() -> loadTexture("main-menu", "splash.png");
+    sf::Sprite splashSprite;
+    splashSprite.setScale((float) windowDim / splash.getSize().x, (float) windowDim / splash.getSize().y);
+    splashSprite.setTexture(splash);
+
+    sf::RenderWindow window(sf::VideoMode(windowDim, windowDim), "(Loading) I Am Synthetic", sf::Style::None);
+    // don't need to clear because sprite takes up entire window
+    window.draw(splashSprite);
+    window.display();
 
     while (window.isOpen()) {
         sf::Event event;
