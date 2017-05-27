3a982c0cb47a50cb7efc8d982ed94144d01ea33c
Cilan <abraham.hamidi01@gmail.com>
2017-05-26 20:10:57 -0500

    Update component system and implement

--- src/game-objects/GameObject.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by cilan on 3/18/2017.
-//
-
-#include "GameObject.h"
-
-GameObject::GameObject() {
-}
--- src/game-objects/components/DrawableComponent.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by cilan on 5/25/2017.
-//
-
-#include "DrawableComponent.h"
