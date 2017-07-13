bea7a7907f543bf461387961420fab02122648cf
Cilan <abraham.hamidi01@gmail.com>
2017-07-13 14:40:19 -0500

    Add Mesh "Library"

---
+++ cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/DependInfo.cmake
@@ -0,0 +1,60 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/Data.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/Data.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/Locator.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/Locator.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/buttons/BackButton.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/buttons/BackButton.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/buttons/Button.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/buttons/Button.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/buttons/CancelTextInputButton.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/buttons/CancelTextInputButton.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/buttons/ConfirmButton.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/buttons/ConfirmButton.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/buttons/CreditsButton.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/buttons/CreditsButton.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/buttons/FileAddButton.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/buttons/FileAddButton.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/buttons/FileButton.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/buttons/FileButton.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/buttons/MainMenuButton.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/buttons/MainMenuButton.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/buttons/PlayButton.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/buttons/PlayButton.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/buttons/PlayLevelButton.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/buttons/PlayLevelButton.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/game-objects/InputTextObject.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/game-objects/InputTextObject.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/game-objects/RectangleObject.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/game-objects/RectangleObject.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/game-objects/ShapeObject.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/game-objects/ShapeObject.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/game-objects/SpriteObject.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/game-objects/SpriteObject.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/game-objects/StoryObject.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/game-objects/StoryObject.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/game-objects/TextObject.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/game-objects/TextObject.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/game-objects/components/Component.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/game-objects/components/Component.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/game-objects/components/RectangleComponent.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/game-objects/components/RectangleComponent.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/game-objects/components/RenderComponent.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/game-objects/components/RenderComponent.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/game-objects/components/ShapeComponent.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/game-objects/components/ShapeComponent.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/game-objects/components/SpriteComponent.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/game-objects/components/SpriteComponent.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/game-objects/components/TextComponent.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/game-objects/components/TextComponent.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/main.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/main.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/resources/LocalResources.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/resources/LocalResources.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/resources/MainMenuTextures.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/resources/MainMenuTextures.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/resources/NullResources.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/resources/NullResources.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/resources/TextureLoader.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/resources/TextureLoader.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/scenes/CreditsScene.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/scenes/CreditsScene.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/scenes/LevelScene.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/scenes/LevelScene.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/scenes/MainMenuScene.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/scenes/MainMenuScene.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/scenes/PlayScene.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/scenes/PlayScene.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/scenes/Scene.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/scenes/Scene.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/scenes/StoryScene.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/scenes/StoryScene.cpp.o"
+  "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/src/scenes/TextInput.cpp" "/home/cilan/Documents/projects/i-am-synthetic-cpp-sfml/cmake/CMakeFiles/i_am_synthetic_c___sfml.dir/src/scenes/TextInput.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_GLIBCXX_USE_CXX11_ABI=0"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
